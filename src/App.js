a59194cf9777a85975469bbad56e53b96ab0af0a
Long Hoang <phl19.dev@gmail.com>
2023-10-29 14:48:00 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
